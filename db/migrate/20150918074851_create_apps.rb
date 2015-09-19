5332f5f27d02b41299020053aab54dfcaa6e4924
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-09-19 19:06:31 +0200

    Users are apps now

---
+++ db/migrate/20150918074851_create_apps.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :apps do |t|
+      t.string :email, null: false
+      t.string :name,  null: false
+      t.string :ssh_key
+      t.text :env_vars
+
+      t.timestamps null: false
+    end
+
+    add_index :apps, :name, unique: true
+  end
+end
