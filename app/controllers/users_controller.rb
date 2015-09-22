7cbb6465284cca63f1f6c2f7eda5e9915cec6220
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-09-22 08:22:46 +0200

    Updates user flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def show
     @user = fetch_user
-    @apps = @user.apps
+    @apps = @user.apps.order(id: :asc)
+  end
+
+  def edit
+    @user = fetch_user
+  end
+
+  def update
+    @user = fetch_user
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def index
@@ -14,5 +27,9 @@ class UsersController < ApplicationController
     User.find(params[:id]).tap do |user|
       redirect_to current_user unless user == current_user
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :ssh_key)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_link_to_li(*args)
+    additional = { wrap_tag: :li, active: :exclusive }
+    opts = if args.last.is_a?(Hash)
+      args.pop.merge(additional)
+    else
+      additional
+    end
+    args.push(opts)
+    active_link_to *args
+  end
 end
