f6cd12cc2eb57452afb9f845c896ee62f06b5ca6
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-09-26 00:57:45 +0200

    Enables bigger log file tailing

--- app/models/app_log_tailer.rb
+++ app/models/app_log_tailer.rb
@@ -3,7 +3,7 @@ class AppLogTailer
     log_path = Rails.root.join("log/#{Rails.env}.log")
     File.exists?(log_path) or return ''
     size = File.size(log_path)
-    size_to_fetch = [size, 1000].min
+    size_to_fetch = [size, 200000].min
     offset = [size-size_to_fetch, size_to_fetch].max
     IO.binread(log_path, size_to_fetch, offset)
   end
