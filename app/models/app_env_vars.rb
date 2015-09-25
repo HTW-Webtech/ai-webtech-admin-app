79e4b0c652c7140f2540ef5722f7d5519c480e2b
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-09-25 07:38:28 +0200

    Removes AppEnvVars code

--- app/models/app_env_vars.rb
+++
@@ -1,8 +0,0 @@
-class AppEnvVars
-  def self.encode(env_vars_from_view)
-    env_vars_from_view.lines.each_with_object({}) do |line, hash|
-      captures = line.strip.match(/^(\w+):\s*([\w-]+)$/i)
-      hash[captures[1].upcase] = captures[2]
-    end
-  end
-end
