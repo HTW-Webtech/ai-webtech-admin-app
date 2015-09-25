ace6d3c7e2efa5be0ee37a3d8cc10cac052b1145
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-09-26 00:58:53 +0200

    Fixes admin app migration

--- db/migrate/20150925050624_load_admin_apps_from_apps_yml.rb
+++ db/migrate/20150925050624_load_admin_apps_from_apps_yml.rb
@@ -1,17 +1,16 @@
 class LoadAdminAppsFromAppsYml < ActiveRecord::Migration
   # Move this into a generic feature?
-  require 'byebug'
   def up
     admin_app = ArisControl::Bookkeeper.new.apps['admin']
-    app = App.create!({
+    App.create!({
       name: 'admin',
       user: User.admin,
       email: admin_app['email'],
       env_vars: admin_app['env_vars'],
       ssh_key: admin_app['ssh_key'],
-      pg_database: admin_app['env_vars']['PG_DB'],
-      pg_login: admin_app['env_vars']['PG_USER'],
-      pg_passwd: admin_app['env_vars']['PG_PASSWORD'],
+      pg_database: admin_app['env_vars']['POSTGRES_DATABASE_NAME'],
+      pg_login: admin_app['env_vars']['POSTGRES_LOGIN'],
+      pg_passwd: admin_app['env_vars']['POSTGRES_PASSWORD'],
     })
   end
 
