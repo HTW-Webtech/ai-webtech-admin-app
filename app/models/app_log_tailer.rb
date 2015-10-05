dc14240a6ade85c8028a4fb1f7525f2a16d6895f
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-10-05 19:00:09 +0200

    LogTailer uses the correct log_path now

--- app/models/app_log_tailer.rb
+++ app/models/app_log_tailer.rb
@@ -1,10 +1,15 @@
 class AppLogTailer
   def self.fetch(app)
-    log_path = Rails.root.join("log/#{Rails.env}.log")
-    File.exists?(log_path) or return ''
-    size = File.size(log_path)
+    path = log_path(app)
+    File.exists?(path) or return ''
+    size = File.size(path)
     size_to_fetch = [size, 200000].min
     offset = [size-size_to_fetch, size_to_fetch].max
-    IO.binread(log_path, size_to_fetch, offset)
+    IO.binread(path, size_to_fetch, offset)
+  end
+
+  # TODO: uargs
+  def self.log_path(app)
+    "/var/apps/#{app.name}/logs/rails.log"
   end
 end
