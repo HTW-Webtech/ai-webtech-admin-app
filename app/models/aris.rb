09d4826c3aba9a4159bf7178bf010a420883d728
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-10-15 23:10:01 +0200

    Fix semaphore writing mechanism

--- app/models/aris.rb
+++ app/models/aris.rb
@@ -5,7 +5,7 @@ class Aris
   end
 
   def self.write_semaphore(app)
-    IO.binwrite("/var/apps/#{app.name}/.reloading")
+    IO.binwrite("/var/apps/#{app.name}/.reloading", "Time: #{Time.now.to_s}")
   end
 
   def self.app_opts(app)
