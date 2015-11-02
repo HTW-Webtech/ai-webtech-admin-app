527973eec4e68adf400c74bd29d759cf5089aef6
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-02 22:04:01 +0100

    Add WIP status page for currently reviewed/green apps per exercise

--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   class StatusController < BaseController
     def show
+      @exercise_ids = App.distinct(:exercise_id).pluck(:exercise_id)
+      @apps = App.all
     end
   end
 end
