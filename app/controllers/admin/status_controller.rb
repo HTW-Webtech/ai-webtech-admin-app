bde79749846c7b830bfe41da2f2143747c7c2b59
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-02 22:05:32 +0100

    Sort exercise overview

--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class StatusController < BaseController
     def show
-      @exercise_ids = App.distinct(:exercise_id).pluck(:exercise_id)
+      @exercise_ids = App.distinct(:exercise_id).pluck(:exercise_id).sort
       @apps = App.all
     end
   end
