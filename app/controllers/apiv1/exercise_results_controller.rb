3afe48d1e992d1020f5f8296ca159857330dca26
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-23 08:26:37 +0100

    Finetune exercise results controller

--- app/controllers/apiv1/exercise_results_controller.rb
+++ app/controllers/apiv1/exercise_results_controller.rb
@@ -1,14 +1,18 @@
 module Apiv1
   class ExerciseResultsController < BaseController
     def create
-      app = App.where(name: params[:app_name]).first!
-      report = JSON.parse(request.body.string)
-      exercise_id = params[:exercise_id]
-      result = ExerciseResult.new(app: app, exercise_id: exercise_id)
-      if result.save!
-        render json: JSON.generate({ status: 'ok' })
+      exercise_result = params[:exercise_result]
+      if exercise_result[:success] == true
+        app = App.where(name: params[:app_name]).first!
+        exercise_id = params[:exercise_id]
+        result = ExerciseResult.new(app: app, exercise_id: exercise_id)
+        if result.save!
+          render json: JSON.generate({ status: 'ok' })
+        else
+          render json: JSON.parse({ status: 'error', result: result.errors.to_s })
+        end
       else
-        render json: JSON.parse({ status: 'error', result: result.errors.to_s })
+        render json: JSON.parse({ status: 'error', result: 'success was false' })
       end
     end
   end
