0c2cb58cb045a25270ed27350845bad31f154337
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-25 06:53:06 +0100

    Drop unique index on App.name and migrate old app names to permalinks

--- db/migrate/20151125045822_remove_app_id_from_name.rb
+++ db/migrate/20151125045822_remove_app_id_from_name.rb
@@ -2,10 +2,16 @@ class RemoveAppIdFromName < ActiveRecord::Migration
   class App < ActiveRecord::Base
   end
 
-  def change
+  def up
+    remove_index :apps, :name
+
     App.find_each do |app|
-      next unless !!app.name(/\d+-\w+/)
+      next unless !!app.name.match(/\d+-\w+/)
       app.update! name: app.name.match(/\d-(.+)/)[1]
     end
+  end
+
+  def down
+    add_index :apps, :name, unique: true
   end
 end
