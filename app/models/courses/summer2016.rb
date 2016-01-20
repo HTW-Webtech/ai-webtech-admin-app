b87b9e53eaff1bac6db9fc29c439ed1f0b48ac05
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-01-20 07:15:32 +0100

    Add empty 2016 summers course

---
+++ app/models/courses/summer2016.rb
@@ -0,0 +1,19 @@
+module Courses
+  module Summer2016
+    module_function
+
+    EXERCISES = {
+    }
+
+    REVIEW_POINTS = {
+    }
+
+    def exercises
+      EXERCISES
+    end
+
+    def review_points
+      REVIEW_POINTS
+    end
+  end
+end
