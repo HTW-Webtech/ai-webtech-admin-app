05d197872478db9f393dbbfa61e1bec4d4b6a4e4
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-01-20 07:39:23 +0100

    Fix access to ExercisePointMaster

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class StudentsController < BaseController
     def show
       @students = User.includes(:apps).all
-      @exercise_ids = ExercisePointMaster.exercise_ids
+      @exercise_ids = ExercisePointMaster.new.exercise_ids
     end
   end
 end
