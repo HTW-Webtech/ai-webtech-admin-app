6298f9fb68af5be5f626e6bf538e0f1303f770d8
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-04-13 16:47:31 +0200

    Feature-Specs: Add first spec for Codereviews

---
+++ spec/features/attending_codereviews_spec.rb
@@ -0,0 +1,10 @@
+# Admin creates N codereviews. N is determined by
+#
+# 17.30-19 Uhr
+# 19.15-20.45 Uhr => 60 / 10 = 6 * 4 = 24
+#
+# A user needs to select a pratice group
+# Once she has selected the group the admin can create a review date
+# The admin creates a group for the review
+# The admin adds the users (at least 3) to the review group
+# The admin creates a review
