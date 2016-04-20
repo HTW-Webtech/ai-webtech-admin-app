2f7b8b53652c305b12a14cc6a96265ee19cf7f54
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-04-20 13:19:29 +0200

    Ensure all users belong to at least one group (e.g. course group)

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -4,6 +4,13 @@ class BaseController < ::ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :redirect_users_to_users_namespace
+  before_action :ensure_user_belongs_to_one_group
+
+  def ensure_user_belongs_to_one_group
+    if current_user && current_user.groups.empty?
+      current_user.groups << Group.current_course
+    end
+  end
 
   def redirect_users_to_users_namespace
     if current_user && !current_user.admin? && !request.path.start_with?("/users/")
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: :groups_users
 
+  def self.current_course
+    where(name: Courses.current.name.demodulize).first_or_create!
+  end
+
   def display_name
     "#{name} (#{users.count})"
   end
