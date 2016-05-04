b3c4a745b7cb739bb5698885355b8eb45f5dae46
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-04 19:46:49 +0200

    Fix typo when creating Jenkins config file

--- app/controllers/admin/jenkins_controller.rb
+++ app/controllers/admin/jenkins_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class JenkinsController < BaseController
     def create
-      apps = Course.current.apps
+      apps = Courses.current.apps
       JenkinsService.publish(apps)
       redirect_to root_path, notice: "Recreated jenkins_yaml for #{apps.count} apps."
     end
