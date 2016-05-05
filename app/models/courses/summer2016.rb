6685db3ff99f0f407b44ae7daaa8bebca4157aa5
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-05 19:27:48 +0200

    Move deadline for 2nd exercise to 6th of may

--- app/models/courses/summer2016.rb
+++ app/models/courses/summer2016.rb
@@ -4,7 +4,7 @@ module Courses
     def self.exercises
       {
         1 => [ 0, Date.new(2016, 12, 12) ],
-        2 => [ 2, Date.new(2016, 5, 5) ], # TODO: Deadline moved to 4th of May
+        2 => [ 2, Date.new(2016, 5, 6) ],
         3 => [ 4, Date.new(2016, 5, 9) ],
         4 => [ 2, Date.new(2016, 5, 16) ],
       }
