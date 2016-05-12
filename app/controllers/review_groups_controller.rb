d33c61edfbd5f6b5f468cfdf2f3c43bfd484b31d
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-12 10:47:49 +0200

    ReviewGroups: Allow users to update order

--- app/controllers/review_groups_controller.rb
+++ app/controllers/review_groups_controller.rb
@@ -2,4 +2,16 @@ class ReviewGroupsController < ::BaseController
   def show
     @review_group = current_user.review_group
   end
+
+  def update
+    review_group = current_user.review_group
+    review_group.update review_group_params
+    redirect_to :back, notice: 'Einstellungen aktualisiert'
+  end
+
+  private
+
+  def review_group_params
+    params.require(:review_group).permit(:order)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]  do
     resources :apps
-    resource :review_group, only: [:show]
+    resource :review_group, only: [:show, :update]
   end
 
   namespace :admin do
