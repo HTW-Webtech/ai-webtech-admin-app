3f5341f4323aea9dd99b007d4422f1db4c7e1419
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-12 10:48:51 +0200

    CodeReview Hints: Move hint from flash message to navigation

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -7,7 +7,6 @@ class BaseController < ::ApplicationController
   before_action :ensure_user_belongs_to_one_group
   before_action :ensure_user_belongs_to_one_course
   before_action :notify_users_without_name
-  before_action :notify_users_about_review_dates
 
   private
 
@@ -26,15 +25,6 @@ class BaseController < ::ApplicationController
   def notify_users_without_name
     if current_user && current_user.name.blank?
       flash[:alert] = "Please set a name on your Profile page"
-    end
-  end
-
-  def notify_users_about_review_dates
-    if current_user
-      ReviewDate.upcoming_for_user(current_user).each do |date|
-        link_to_info = "<a href='https://portal.htw-webtech.com/site/courses/ss2016/index.html#code-review'>Mehr Infos zum CodeReview-Termin.</a>"
-        add_flash type: :notice, message: "Du hast einen CodeReview-Termin (#{date.id}) für die #{date.exercise_id}te Aufgabe am #{date.begins_at.to_s(:long)} Uhr. Geplante Dauer: #{date.length_in_minutes} Min. #{link_to_info}.<br/>Wenn Eure Gruppe an dem Termin nicht teilnehmen kannst, meldet euch per E-Mail/Slack."
-      end
     end
   end
 
